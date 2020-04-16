1566133f346263e72b2cfd3535ab73946ab994cb
Christian Bitter <christian.bitter@gmail.com>
2020-04-16 20:45:11 +0200

    Added car park

--- R/common.R
+++ R/common.R
@@ -3,8 +3,8 @@ base_request <- function(url, ...) {
   verbose <- F;
   execute <- T;
 
-  .args <- list(...);
-  .nargs<- names(.args);
+  .args  <- list(...);
+  .nargs <- names(.args);
 
   if ("verbose" %in% .nargs) verbose <- .args[["verbose"]];
   if ("execute" %in% .nargs) execute <- .args[["execute"]];
@@ -17,7 +17,7 @@ base_request <- function(url, ...) {
     r <- httr::GET(url = url);
 
     if (httr::status_code(r) != 200) {
-      stop(sprintf("lift - failed with\r\n%s", str(http_status(r))));
+      stop(sprintf("lift - failed with\r\n%s", str(httr::http_status(r))));
     }
 
     content <- httr::content(r, encoding = "UTF-8", as = "text");
@@ -27,32 +27,38 @@ base_request <- function(url, ...) {
   return(json_content);
 }
 
-extract_geom <- function(data_df) {
+extract_geom <- function(data_df, pull_from_geometry = T) {
   if (missing(data_df)) stop("data missing");
   if (nrow(data_df) < 1) stop("No data");
-  if (!("geometry" %in% names(data_df))) stop("no geometry attribute in data");
 
-  coords  <- data_df$geometry$coordinates;
-  coords  <- unlist(coords);
-  coords_m<- matrix(data = coords, ncol = 2, byrow = T);
-  data_df$x <- coords_m[, 1];
-  data_df$y <- coords_m[, 2];
-  data_df$geom_type <- data_df$geometry$type;
+  if (pull_from_geometry) {
+    if (!("geometry" %in% names(data_df))) stop("no geometry attribute in data");
 
+    coords   <- data_df$geometry$coordinates;
+    coords   <- unlist(coords);
+    coords_m <- matrix(data = coords, ncol = 2, byrow = T);
+    data_df$x <- coords_m[, 1];
+    data_df$y <- coords_m[, 2];
+    data_df$geom_type <- data_df$geometry$type;
+
+  } else {
+    data_df$x <- data_df$geometry$x;
+    data_df$y <- data_df$geometry$y;
+  }
   data_df$geometry <- NULL;
-
   return(data_df);
 }
 
-extract_prop <- function(data_df) {
+extract_prop <- function(data_df, properties_attrib_name = "properties") {
   if (missing(data_df)) stop("data missing");
   if (nrow(data_df) < 1) stop("No data");
-  if (!("properties" %in% names(data_df))) stop("no properties attribute in data");
+  if (is.null(properties_attrib_name) || nchar(properties_attrib_name) < 1) stop("properties attribute name not specified");
+  if (!(properties_attrib_name %in% names(data_df))) stop("no properties attribute in data");
 
-  ni                 <- names(data_df$properties);
-  di                 <- data.frame(data_df$properties[1:length(ni)]);
+  ni                 <- names(data_df[[properties_attrib_name]]);
+  di                 <- data.frame(data_df[[properties_attrib_name]][1:length(ni)]);
   data_df            <- cbind(data_df, di);
-  data_df$properties <- NULL;
+  data_df[[properties_attrib_name]] <- NULL;
   return(data_df);
 }
 
--- R/traffic.R
+++ R/traffic.R
@@ -1,6 +1,40 @@
 traffic <- function(){
   url <- "https://www.stadt-koeln.de/externe-dienste/open-data/traffic.php";
   json_content <- base_request(url = url);
+  epsg_code    <- json_content$spatialReference$wkid;
+  data_df <- json_content$features;
+  data_df <- rOCologneKVB::extract_prop(data_df, properties_attrib_name = "attributes");
+  data_df$paths <- data_df$geometry$paths;
+  data_df$geometry <- NULL;
+  data_sf  <- sf::st_as_sfc(data_df, agr = "geometry", crs = epsg_code);
+
+  # we have to walk through all line strings ... and construct them
+
+
   # TODO: prep
   return(json_content);
+}
+
+#'@author christian bitter
+#'@name car_park
+#'@title Cologne City - car park occupancy
+#'@description Returns the status of car parks around cologne. (see: http://www.stadt-koeln.de/externe-dienste/open-data/parking-ts.php)
+#'@return an sf object of the provided data (EPSG 4326). Containing attributes
+#'IDENTIFIER: identifier of the car park (e.g., PH01 Cologne Central Station)
+#'KAPAZITAET: number of available parking spots ( -1 = no data available, service interruption).
+#'TENDENZ: -1 => not available 0 => number of parking spots constant 1 => less spots 2 => more parking places
+#'@examples
+#'sp_sf <- car_park()
+#'@export
+car_park <- function(){
+  url <- "https://www.stadt-koeln.de/externe-dienste/open-data/parking-ts.php";
+  json_content <- base_request(url = url);
+  epsg_code    <- json_content$spatialReference$wkid;
+  data_df <- json_content$features;
+  data_df <- rOCologneKVB::extract_geom(data_df, pull_from_geometry = F);
+  data_df <- rOCologneKVB::extract_prop(data_df, properties_attrib_name = "attributes");
+  data_sf  <- sf::st_as_sf(data_df, coords = c("x", "y"))
+  sf::st_crs(data_sf) <- epsg_code;
+  data_sf <- sf::st_transform(data_sf, crs = 4326);
+  return(data_sf);
 }
